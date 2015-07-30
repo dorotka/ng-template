4c376386e5e5f309abab80e7dfe4ad1f2fbcd8e3
Dorotka <dorota_oleszczuk@yahoo.com>
2015-07-30 19:22:30 -0400

    Added angular strap, footer in index and common.scss file

--- src/public/scripts/app/home/controllers/HomeCtrl.js
+++ src/public/scripts/app/home/controllers/HomeCtrl.js
@@ -3,7 +3,7 @@ var home = angular.module('home');
 function HomeCtrl($scope) {
     $scope.homeing = 'home';
     console.log("homeing", $scope.homeing);
-    $scope.message = 'Everyone come and see how good I look!';
+    $scope.message = 'This is a very important message!';
 }
 
 home.controller('HomeCtrl', ['$scope', HomeCtrl]);
